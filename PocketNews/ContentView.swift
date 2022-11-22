9d2710936c65f809f1a13ebe5efac6d26fd79040
vaismanovich <80892801+vaismanovich@users.noreply.github.com>
2022-11-22 22:10:19 +0200

    Initial Commit

---
+++ PocketNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PocketNews
+//
+//  Created by Мария Шувалова on 22.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PocketNews/PocketNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketNewsApp.swift
+//  PocketNews
+//
+//  Created by Vitaliy Vaisman on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PocketNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
