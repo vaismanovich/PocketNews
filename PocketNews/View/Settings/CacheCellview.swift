6bad29e5cf234713f11dc6aa2fa47f1cd8de3fd8
vaismanovich <80892801+vaismanovich@users.noreply.github.com>
2022-12-08 22:00:06 +0200

    add CacheCellView

---
+++ PocketNews/View/Settings/CacheCellview.swift
@@ -0,0 +1,34 @@
+//
+//  CacheCellview.swift
+//  PocketNews
+//
+//  Created by Мария Шувалова on 08.12.2022.
+//
+
+import SwiftUI
+
+struct CacheCellview: View {
+    let title: String
+    let size:  String
+    let action: () -> Void
+    
+    var body: some View {
+        HStack {
+            Text(title)
+            Text(size)
+            Spacer()
+            Button("DELETE", role: .destructive, action: action)
+                .background(.cyan)
+                .shadow(radius: 7)
+                
+            
+            
+        }
+    }
+}
+
+struct CacheCellview_Previews: PreviewProvider {
+    static var previews: some View {
+        CacheCellview(title: "Images", size: "20 MB", action:{})
+    }
+}
